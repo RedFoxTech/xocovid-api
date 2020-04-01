a53e51000fd87cf713db433b0dbc12fc5060900c
REDFOX <REDFOX@MacBook-Pro-de-Leo.local>
2020-04-01 16:51:52 -0300

    add user object for return singin

--- src/mappers/user.js
+++ src/mappers/user.js
@@ -6,9 +6,4 @@ exports.userMapper = ({ _id, name, age, email, password }) => ({
     password
 })
 
-exports.userMapperSignIn = ({ _id, name, age, email, password }) => ({
-    _id,
-    name,
-    age,
-    email
-})
+exports.userMapperSignIn = ({ password, ...user }) => user
